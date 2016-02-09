e1d4d2ee32c990bdd42f689824bf68aeac5269f7
Matthew Hartley <mhartley@cantab.net>
2016-02-09 14:21:15 +0000

    Initial commit

---
+++ scripts/read_isq.py
@@ -0,0 +1,83 @@
+"""Read ISQ file."""
+
+import os
+import io
+import argparse
+
+import numpy as np
+
+import scipy.misc
+
+from skimage.filters import sobel
+
+# 44 bytes
+# 24 bytes stuff
+# 440 bytes
+# 3 bytes stuff
+
+
+def bytes_to_int(bytes):
+
+    value = 0
+
+    for n, b in enumerate(bytes):
+        value += ord(b) << (n * 8)
+
+    return value
+
+def dump_bytes(bytes):
+
+    for n, b in enumerate(bytes):
+        print n, ord(b)
+
+def read_isq_file(filename):
+    """Read an ISQ file."""
+
+    with io.open(filename, 'rb') as f:
+        header_bytes = f.read(2048)
+
+        xdim = bytes_to_int(header_bytes[44:47])
+        ydim = bytes_to_int(header_bytes[48:51])
+        zdim = bytes_to_int(header_bytes[52:55])
+
+        tmp_int = bytes_to_int(header_bytes[56:60])
+
+        #more_bytes = f.read(xdim * ydim * 2 * zdim)
+        more_bytes = f.read(xdim * ydim * 2 * 600)
+
+        im_array = np.fromstring(more_bytes, dtype='<i2')
+
+    nim = np.reshape(im_array, (xdim, ydim, -1), order='F')
+ 
+
+    return nim
+
+def imsave_signed(filename, array):
+
+    array = np.maximum(array, 0)
+    scipy.misc.imsave(filename, array)
+
+def spike_ct(filename):
+
+    image = read_isq_file(filename)
+
+    #90 - 170
+
+    # 279 - 575 for large
+
+    path = 'stacklet'
+
+    for z in range(279, 575):
+        filename = os.path.join(path, 'sample_Z{}.png'.format(str(z)))
+        imsave_signed(filename, image[:,:,z])
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename')
+
+    args = parser.parse_args()
+
+    spike_ct(args.filename)
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/stacktools.py
@@ -0,0 +1,190 @@
+"""Tools and tests for operating on 3D stacks."""
+
+import re
+import os
+
+import numpy as np
+
+import scipy.misc
+
+import skimage.draw
+import skimage.measure
+from skimage.filters import threshold_otsu
+
+from scipy.ndimage.morphology import binary_erosion
+
+from jicbioimage.core.image import Image
+
+IMAGE_EXTS = ['.png', '.tif', '.tiff']
+HERE = os.path.dirname(__file__)
+OUTPUT = os.path.join(HERE, '..', 'output')
+
+def sorted_nicely( l ):
+    """ Sort the given iterable in the way that humans expect."""
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
+    return sorted(l, key = alphanum_key)
+
+def is_image_filename(filename):
+    """Decide whether a filename is an image based on a list of allowed
+    extensions."""
+
+    base, ext = os.path.splitext(filename)
+
+    if ext in IMAGE_EXTS:
+        return True
+
+    return False
+
+def test_is_image_filename():
+
+    image_filename = 'test.png'
+    not_an_image = 'test.txt'
+
+    assert( is_image_filename(image_filename) )
+    assert( not is_image_filename(not_an_image) )
+
+def section_stack(input_stack):
+
+    x, y = 1250, 1000
+    xdim, ydim = 500, 500
+
+    return input_stack[x:x+xdim,y:y+ydim,:]
+
+def save_stack(stack_name, stack):
+
+    stack_dir = os.path.join(OUTPUT, stack_name + '.stack')
+
+    if not os.path.isdir(stack_dir):
+        os.mkdir(stack_dir)
+
+    xdim, ydim, zdim = stack.shape
+
+    for z in range(zdim):
+        filename = 'z{}.png'.format(z)
+        full_name = os.path.join(stack_dir, filename)
+        scipy.misc.imsave(full_name, stack[:,:,z])
+
+def imsave(filename, image):
+    
+    full_path = os.path.join(OUTPUT, filename)
+
+    scipy.misc.imsave(full_path, image)
+    
+def spike_stack():
+
+    input_stack_path = 'stacklet'
+
+    all_files = os.listdir(input_stack_path)
+    image_files = filter(is_image_filename, all_files)
+    sorted_image_files = sorted_nicely(image_files)
+    full_image_paths = [os.path.join(input_stack_path, fn) 
+                        for fn in sorted_image_files]
+
+    all_images = [Image.from_file(fn) for fn in full_image_paths]
+
+    stack = np.dstack(all_images)
+
+    sectioned_stack = section_stack(stack)
+
+    save_stack('sectioned', sectioned_stack)
+
+def segment_it():
+    input_stack_path = 'sectioned.stack'
+
+    all_files = os.listdir(input_stack_path)
+    image_files = filter(is_image_filename, all_files)
+    sorted_image_files = sorted_nicely(image_files)
+    full_image_paths = [os.path.join(input_stack_path, fn) 
+                        for fn in sorted_image_files]
+
+    all_images = [Image.from_file(fn) for fn in full_image_paths]
+
+    stack = np.dstack(all_images)
+
+    tval = threshold_otsu(stack)
+
+    thresholded_stack = stack > tval
+
+    #save_stack('thresh', thresholded_stack)
+
+    selem = np.ones((5, 5, 5))
+
+    eroded = binary_erosion(thresholded_stack, structure=selem)
+
+    #save_stack('eroded', eroded)
+
+    ccs = skimage.measure.label(eroded)
+
+    labels = np.unique(ccs)
+
+    # print labels
+
+    # def label_size(l):
+    #     return len(np.where(ccs==l)[0])
+
+    # sizes = [(label_size(l), l) for l in labels]
+
+    # print sorted(sizes, reverse=True)
+
+    #imsave('lablet.png', ccs[:,:,100])
+
+    print ccs[250, 250, 100]
+    seed = np.where(ccs == 148)
+
+    print len(seed[0])
+
+def spike_stuff():
+    
+    example_fn = 'sectioned.stack/z100.png'
+
+    start_slice = Image.from_file(example_fn)
+
+    imsave('start_slice.png', start_slice)
+
+    line_start = 10, 10
+    line_end = 490, 490
+
+    pline = skimage.measure.profile_line(start_slice, line_start, line_end,
+                                         order=0)
+
+    print np.min(pline), np.max(pline)
+
+    graphlet = np.zeros((200, 800), dtype=np.uint8)
+
+    for n, h in enumerate(pline):
+        graphlet[199-h, n] = 255
+
+    imsave('graphlet.png', graphlet)
+
+    rr, cc = skimage.draw.line(*(line_start + line_end))
+
+    start_slice[rr, cc] = 255
+
+    imsave('annotated_image.png', start_slice)
+
+    enumerated = list(enumerate(pline))
+
+    with open('dump.txt', 'w') as f:
+        f.write('position,intensity\n')
+        values_string = '\n'.join('{},{}'.format(*v) for v in enumerated)
+        f.write(values_string)
+
+def main():
+
+    # single = 'stacklet/sample_Z300.png'
+    # im = Image.from_file(single)
+
+    # x, y = 1250, 1000
+    # xdim, ydim = 500, 500
+    # section = im[x:x+xdim,y:y+ydim]
+    # imsave('section.png', section)
+    
+    #spike_stack()
+
+    #spike_stuff()
+
+    segment_it()
+
+if __name__ == "__main__":
+    main()
